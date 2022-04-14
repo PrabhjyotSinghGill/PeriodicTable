fec94ead5bd6067558d37c261a98252b3476567c
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-14 18:32:17 -0400

    created ElementDefinition component

---
+++ src/ElementDefinition/ElementDefinition.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./ElementDefinition.css";
+
+function ElementDefinition() {
+    return <div className='elementDefinition'></div>;
+}
+
+export default ElementDefinition;
